8899bfcbaedef8fbfcb42d574387738319245882
global <global>
2025-04-27 22:43:32 +0700

    moved to msvc.

--- main.cpp
+++ main.cpp
@@ -1,18 +1,36 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <cmath>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <unordered_set>
+#include <queue>
+#include <stack>
+#include <deque>
+#include <numeric>
+#include <bitset>
+#include <limits>
+#include <iomanip>
+#include <sstream>
+#include <tuple>
+#include <functional>
 #define all(a) (a).begin(), (a).end()
 using ll = long long;
 using ull = unsigned long long;
 using ld = long double;
 using dd = double;
 using fl = float;
-template <typename T> using v = std::vector<T>;
-template <typename T> using umset = std::unordered_multiset<T>;
-template <typename T> using mset = std::multiset<T>;
-template <typename T> using uset = std::unordered_set<T>;
-template <typename T, typename F> using p = std::pair<T, F>;
-template <typename T, typename F> using umap = std::unordered_map<T, F>;
-template <typename T, typename F> using mmap = std::multimap<T, F>;
 using namespace std;
+template <typename T> using v = vector<T>;
+template <typename T> using umset = unordered_multiset<T>;
+template <typename T> using mset = multiset<T>;
+template <typename T> using uset = unordered_set<T>;
+template <typename T, typename F> using p = pair<T, F>;
+template <typename T, typename F> using umap = unordered_map<T, F>;
+template <typename T, typename F> using mmap = multimap<T, F>;
 char el = '\n';
 
 void open(const string input, const string output) {
@@ -29,4 +47,5 @@ void desync() {
 
 int main() {
   desync();
+  cout << "Hello World";
 }
